baa14b3ded19d549d765b707ace90218e7420631
Rahmat Kurniawan <rahmat@Rahmats-MacBook-Air.local>
2023-12-04 16:08:14 +0700

    micro feature laravel

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('alamat');
+            $table->string('jenis_kelamin');
+            $table->string('username');
+            $table->string('password');
+            $table->string('role');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name');
+        //     $table->string('email')->unique();
+        //     $table->timestamp('email_verified_at')->nullable();
+        //     $table->string('password');
+        //     $table->rememberToken();
+        //     $table->timestamps();
+        // });
+        // Schema::create('password_resets', function (Blueprint $table) {
+        //     $table->string('email')->index();
+        //     $table->string('token');
+        //     $table->timestamp('created_at')->nullable();
+        // });
+        // Schema::create('failed_jobs', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('uuid')->unique();
+        //     $table->text('connection');
+        //     $table->text('queue');
+        //     $table->longText('payload');
+        //     $table->longText('exception');
+        //     $table->timestamp('failed_at')->useCurrent();
+        // });
+        // Schema::create('personal_access_tokens', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->morphs('tokenable');
+        //     $table->string('name');
+        //     $table->string('token', 64)->unique();
+        //     $table->text('abilities')->nullable();
+        //     $table->timestamp('last_used_at')->nullable();
+        //     $table->timestamps();
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
